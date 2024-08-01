fb4327144efb4c32153dbcec62e96a1a0260e84b
Diego Sebastian Markiewicz <dieghard@gmail.com>
2024-08-01 11:07:55 -0300

    modificacion en swUtuils

--- js/sw-utils.js
+++ js/sw-utils.js
@@ -1,6 +1,6 @@
 //guardar el cache dinamico
 function actualizaCacheDinamico(dynamicCache, req, res) {
-	if (res.ok) {
+	if (res.ok && (req.url.startsWith("http") || req.url.startsWith("https"))) {
 		return caches.open(dynamicCache).then((cache) => {
 			cache.put(req, res.clone());
 			return res.clone();
